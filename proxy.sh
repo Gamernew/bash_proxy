c73916d1294a2240ed7c9e9241f8436413fd70a6
jessy hurbain <jessyhurbain@gmail.com>
2015-11-10 13:59:58 +0100

    La base

---
+++ proxy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "script installation et désintallation de la configuration du proxy à utiliser en (Root) crée par gamernew"
+
+cd /etc/apt/
+res=$(cat apt.conf)
+if [ "$res" = '# Acquire::http::Proxy "http://192.168.1.34:3142";' ];then
+  echo -n "Voulez-vous mettre le proxy (Y/N) : "
+  read ouinon
+  if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
+    echo 'Acquire::http::Proxy "http://192.168.1.34:3142";' > apt.conf
+    echo "Le proxy à bien était mis à jour"
+  else
+    echo "Vous n'avez pas fait de modification"
+  fi
+else
+  echo -n "Vous avez enlever le proxy (Y/N) : "
+  read ouinon
+  if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
+  echo '# Acquire::http::Proxy "http://192.168.1.34:3142";' > apt.conf
+  echo "Le proxy à bien était mis à jour"
+  else
+    echo "Vous n'avez pas fait de modification"
+  fi
+fi
